71618a4ea997f3c420eb7718892a1c0cff9e9fc7
Maria <mariabrock414@gmail.com>
2020-01-04 14:06:46 -0600

    added info about dogs to data

---
+++ src/Components/helpers/data/dogsData.js
@@ -0,0 +1,53 @@
+const dogs = [
+  {
+    id: 'dog1',
+    imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.vets4pets.com%2Fsiteassets%2Fspecies%2Fdog%2Fclose-up-of-dog.jpg%3Fwidth%3D1040&f=1&nofb=1',
+    name: 'Bradley',
+    owner: 'Buster Harrington',
+    description: 'Bradley is the smiley-est pup you will ever meet. Be prepared for him to lick your feet!',
+  },
+
+  {
+    id: 'dog2',
+    imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.petmd.com%2Fsites%2Fdefault%2Ffiles%2Fshutterstock_121492048.jpg&f=1&nofb=1',
+    name: 'Dudley',
+    owner: 'Marilynn Dunst',
+    description: 'Dudley likes ling walks on the beach, pedicures, and naps in the sun. Dudley is mostly a cat.',
+  },
+
+  {
+    id: 'dog3',
+    imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.pets4homes.co.uk%2Fimages%2Fbreeds%2F147%2F6e0b4bef69ad7fee0325400c9d2c5463.jpg&f=1&nofb=1',
+    name: 'Spot',
+    owner: 'Sir Manly German',
+    description: 'There has never been a more majestic hunting dog than my Spot.',
+  },
+
+  {
+    id: 'dog4',
+    imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.blmH880Ccp-BlMskJct9WwHaEK%26pid%3DApi&f=1',
+    name: 'Bert',
+    owner: 'Miss Eliza Bundt',
+    description: 'I loose Bert in the couch constantly. I think he goes there to hide from the cats.',
+  },
+
+  {
+    id: 'dog5',
+    imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.littlethings.com%2Fapp%2Fuploads%2F2017%2F05%2Fcool-dog-names-five-600x600.jpg&f=1&nofb=1',
+    name: 'Max',
+    owner: 'My Maxxie is an absolute fashionista! He will make it big on th runway one day.',
+    description: 'Mrs. Dahlia Wisely',
+  },
+
+  {
+    id: 'dog6',
+    imageUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.thethings.com%2Fwp-content%2Fuploads%2F2016%2F03%2FFunny_looking_dog.jpeg&f=1&nofb=1',
+    name: 'Charlie',
+    owner: 'Count of the Mountian of Dew',
+    description: 'Derp.',
+  },
+];
+
+const getDogs = () => dogs;
+
+export default { getDogs };
